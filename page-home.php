9b80d40f49f72c27c12a9bb02f51577f0d2cb27f
Luis Carlos Pando <hey@luiscarlospando.com>
2025-10-28 17:13:43 -0600

    Ajustes cliente

--- page-home.php
+++ page-home.php
@@ -97,7 +97,7 @@ get_header(); ?>
             >
                 <div class="row mb-4">
                     <div
-                        class="col-10 offset-1 col-md-9 offset-md-1 col-lg-6 offset-lg-2"
+                        class="col-10 offset-1 col-md-9 offset-md-1 col-lg-8 offset-lg-2"
                     >
                         <div class="row">
                             <div class="col-12 col-md-6">
